5c0cb6344619f2992c84d2faa5abb74cc336b14c
Oliver Gu <gu.oliver@yahoo.com>
2015-04-06 08:43:31 +0200

    Use pipe operator to reduce parentheses

--- examples/buy_market_order.ml
+++ examples/buy_market_order.ml
@@ -15,11 +15,11 @@ let () =
       Common.with_tws ~do_log ~host ~port ~client_id (fun tws ->
         don't_wait_for (
           Pipe.iter_without_pushback (Tws.executions tws) ~f:(fun exec ->
-            printf "%s\n\n%!" (Sexp.to_string_hum (Execution.sexp_of_t exec)))
+            printf "%s\n\n%!" (Execution.sexp_of_t exec |> Sexp.to_string_hum))
         );
         don't_wait_for (
           Pipe.iter_without_pushback (Tws.commissions tws) ~f:(fun comm ->
-            printf "%s\n\n%!" (Sexp.to_string_hum (Commission.sexp_of_t comm)))
+            printf "%s\n\n%!" (Commission.sexp_of_t comm |> Sexp.to_string_hum))
         );
         Tws.submit_order_exn tws
           ~order:(Order.buy_market ~quantity:100)
@@ -38,7 +38,8 @@ let () =
         Tws.portfolio_updates_exn tws
         >>= fun updates ->
         Pipe.iter_without_pushback updates ~f:(fun update ->
-          print_endline (Sexp.to_string_hum (Portfolio_update.sexp_of_t update));
+          print_endline (Portfolio_update.sexp_of_t update |> Sexp.to_string_hum)
         )
-      ))
+      )
+    )
   |> Command.run
